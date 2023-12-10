88f86e08fd185b7976367acc01b4ac53bd4832d0
m091u <mirabelatiugan@gmail.com>
2023-12-11 00:39:05 +0100

    trials to fix deployment bugs

--- readCycle-client/src/context/auth.context.jsx
+++ readCycle-client/src/context/auth.context.jsx
@@ -40,6 +40,7 @@ function AuthProviderWrapper(props) {
         .then((response) => {
           // If the server verifies that the JWT token is valid
           const user = response.data;
+          console.log('Value of user:', user);
           // Update state variables
           setIsLoggedIn(true);
           setIsLoading(false);
